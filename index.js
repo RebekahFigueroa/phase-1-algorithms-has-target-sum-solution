7225c672d4e809c1bd5788ba6823b7b1c39a671e
Rebekah <figueroarebekah@gmail.com>
2023-01-11 15:55:21 -0600

    commit tree dd740b6e3f687097a1e7146d3e0d9a8e170b252e

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+	// Write your algorithm here
+	for (let i = 0; i < array.length; i++) {
+		const complement = target - array[i];
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] === complement) return true;
+		}
+	}
+	return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+// hasTargetSum([22, 19, 4, 6, 30], 25)
+// iterate through each number in the array
+// for the currentnumber, identify compliment that adds to the target
+// (compliment = target-num)
+// iterate through rest of array
+// check if any number is our complement
+// if so, return true
+// if I reach end of array, return false
 
 /* 
   Add your pseudocode here
@@ -16,19 +31,19 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+	console.log("Expecting: false");
+	console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
